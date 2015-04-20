dcfeeefcd7d6c58eec0588385f7da54143e60b7f
martinsandholt <martin@sandholt.me>
2015-04-20 22:02:23 +0200

    Fix jquery poster image hack.

--- src/js/main.js
+++ src/js/main.js
@@ -5,14 +5,5 @@ function is_touch_device() {
 };
 
 if (is_touch_device() === true) {
-  $(".video video").remove();
-  $(".video").css({
-    "height" : "300px",
-    "width" : "auto",
-    "background-color" : "#394555",
-    "background-repeat" : "no-repeat",
-    "background-position" : "center",
-    "background-image" : "url('img/poster.png')",
-    "background-size" : "cover"
-  });
+  $(".video video").replaceWith( "<div class='poster'></div>" );
 }
